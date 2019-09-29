7dcff0061690965e1cb4f25de6f2882fd76ff6b6
flylxz <45684334+flylxz@users.noreply.github.com>
2019-09-29 12:34:24 +0300

    Add files via upload

---
+++ kanbanReactJS/precache-manifest.b2f023e2f88b3a06e6ad47b28a6ec374.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67f157f784f089cbd70a7f28051a87d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "162b46789d0eb2835140",
+    "url": "/static/css/main.f274b86d.chunk.css"
+  },
+  {
+    "revision": "2b8ad867dfd3dda9d0aa",
+    "url": "/static/js/2.a1c3ea4b.chunk.js"
+  },
+  {
+    "revision": "162b46789d0eb2835140",
+    "url": "/static/js/main.796754d4.chunk.js"
+  },
+  {
+    "revision": "2729cff26c9be2b9c51e",
+    "url": "/static/js/runtime~main.91b4beff.js"
+  }
+]);
